215b2aed56e5c78ea6a567f7069604effa45e7ac
Khelsai Rajwade <khelsai@gmail.com>
2024-01-12 16:41:37 +0530

    nyka filter

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -4,4 +4,4 @@ const errorHandler=((err, req, res, next) => {
     res.status(400).json({ error: 'Something went wrong!' });
   });
 
-  module.exports = errorHandler;
+  module.exports = errorHandler
